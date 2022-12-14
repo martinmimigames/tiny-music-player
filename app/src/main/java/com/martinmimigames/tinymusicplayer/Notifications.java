e4ea50ae9962206629b6012a31477f10a056dc33
wmartinmimi <martinmimiw@gmail.com>
2022-12-14 20:53:18 +0000

    reducing code complexity

--- app/src/main/java/com/martinmimigames/tinymusicplayer/Notifications.java
+++ app/src/main/java/com/martinmimigames/tinymusicplayer/Notifications.java
@@ -56,6 +56,24 @@ public class Notifications {
     update();
   }
 
+  PendingIntent genIntent(int id, byte action) {
+    /* flags for control logics on notification */
+    int pendingIntentFlag = PendingIntent.FLAG_IMMUTABLE;
+    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.CUPCAKE) {
+      pendingIntentFlag |= PendingIntent.FLAG_UPDATE_CURRENT;
+    }
+    int intentFlag = Intent.FLAG_ACTIVITY_NO_HISTORY;
+    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.ECLAIR) {
+      intentFlag |= Intent.FLAG_ACTIVITY_NO_ANIMATION;
+    }
+    return PendingIntent
+      .getService(service, id, new Intent(service, Service.class)
+          .addFlags(intentFlag)
+          .putExtra(ServiceControl.TYPE, action)
+          .putExtra(ServiceControl.SELF_IDENTIFIER, ServiceControl.SELF_IDENTIFIER_ID)
+        , pendingIntentFlag);
+  }
+
   /**
    * create and start playback control notification
    */
@@ -63,9 +81,10 @@ public class Notifications {
 
     /* setup notification variable */
     final String title = new File(uri.getPath()).getName();
+
     notification = NotificationHelper.createNotification(service, NOTIFICATION_CHANNEL, (Build.VERSION.SDK_INT > 21) ? android.app.Notification.CATEGORY_SERVICE : null);
+
     notification.icon = R.drawable.ic_notif; // icon display
-    //notification.largeIcon = BitmapFactory.decodeResource(this.getResources(), R.drawable.ic_launcher); // large icon display
     notification.defaults = Notification.DEFAULT_ALL; // set defaults
     notification.when = System.currentTimeMillis(); // set time of notification
     notification.tickerText = title;// set popup text
@@ -73,36 +92,9 @@ public class Notifications {
     notification.audioStreamType = Notification.STREAM_DEFAULT;
     notification.sound = null;
 
-    /* flags for control logics on notification */
-    final int pendingIntentFlag =
-      (Build.VERSION.SDK_INT >= Build.VERSION_CODES.CUPCAKE) ?
-        (PendingIntent.FLAG_UPDATE_CURRENT |
-          ((Build.VERSION.SDK_INT > 23) ?
-            PendingIntent.FLAG_IMMUTABLE : 0)
-        ) : 0;
+
     /* calls for control logic by starting activity with flags */
-    final PendingIntent killIntent =
-      PendingIntent
-        .getService(
-          service,
-          1,
-          new Intent(
-            service,
-            Service.class
-          ).addFlags(
-            Intent.FLAG_ACTIVITY_NO_HISTORY |
-              (
-                (Build.VERSION.SDK_INT >= Build.VERSION_CODES.ECLAIR) ?
-                  Intent.FLAG_ACTIVITY_NO_ANIMATION : 0)
-          ).putExtra(
-            ServiceControl.SELF_IDENTIFIER,
-            ServiceControl.SELF_IDENTIFIER_ID
-          ).putExtra(
-            ServiceControl.TYPE,
-            ServiceControl.KILL
-          ),
-          pendingIntentFlag
-        );
+    final PendingIntent killIntent = genIntent(1, ServiceControl.KILL);
 
     /* extra variables for notification setup */
     /* different depending on sdk version as they require different logic */
@@ -111,16 +103,7 @@ public class Notifications {
       notification.extras.putCharSequence(Notification.EXTRA_TITLE, title);
       notification.extras.putCharSequence(Notification.EXTRA_TEXT, "Tap to stop");
 
-      notification.contentIntent =
-        PendingIntent
-          .getService(
-            service,
-            2,
-            new Intent(service, Service.class)
-              .addFlags(Intent.FLAG_ACTIVITY_NO_ANIMATION | Intent.FLAG_ACTIVITY_NO_HISTORY)
-              .putExtra(ServiceControl.TYPE, ServiceControl.PLAY_PAUSE)
-              .putExtra(ServiceControl.SELF_IDENTIFIER, ServiceControl.SELF_IDENTIFIER_ID)
-            , pendingIntentFlag);
+      notification.contentIntent = genIntent(2, ServiceControl.PLAY_PAUSE);
 
       notification.actions = new Notification.Action[]{
         new Notification.Action(R.drawable.ic_launcher, "close", killIntent)
